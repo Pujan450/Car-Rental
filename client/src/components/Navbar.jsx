95f9896b03d811cddc184378f262189955a66981
Pujan450 <pujandesai450@gmail.com>
2025-12-29 18:49:04 +0530

    Navbar and vercel.jason

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -44,11 +44,16 @@ const Navbar = () => {
 
       {/* Navigation links */}
       <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${location.pathname === "/" ? "bg-light" : "bg-white"} ${open ? "max-sm:translate-x-0":"max-sm:translate-x-full"}`}>
-        {menuLinks.map((link, index) => (
-          <Link key={index} to={link.path}>
-            {link.name}
-          </Link>
-        ))}
+      {menuLinks.map((link, index) => (
+  <Link
+    key={index}
+    to={link.path}
+    onClick={() => setOpen(false)}
+  >
+    {link.name}
+  </Link>
+))}
+
 
         <div className='hidden lg:flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full max-w-56'>
           <input type="text" className='py-1.5 w-full bg-transparent outline-none placeholder-gray-500' placeholder='Search products'/>
@@ -57,9 +62,26 @@ const Navbar = () => {
 
         <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
 
-          <button onClick={()=>isOwner ? navigate('/owner') : changeRole()} className='cursor-pointer'>{isOwner ? 'Dashboard' : 'List cars'}</button>
+        <button
+  onClick={() => {
+    setOpen(false);
+    isOwner ? navigate('/owner') : changeRole();
+  }}
+  className="cursor-pointer"
+>
+  {isOwner ? 'Dashboard' : 'List cars'}
+</button>
 
-          <button onClick={()=> {user ? logout() : setShowLogin(true)}} className='cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg'>{user ? 'Logout' : 'Login'}</button>
+<button
+  onClick={() => {
+    setOpen(false);
+    user ? logout() : setShowLogin(true);
+  }}
+  className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg"
+>
+  {user ? 'Logout' : 'Login'}
+</button>
+
         </div>
 
       </div>
